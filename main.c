7afdeaaf10e700a3c3d304e7470738b00443b784
chao <chao@qq.com>
2019-01-15 11:51:48 +0800

    第一次操作

---
+++ main.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int main()
+{
+
+    printf("进入系统");
+
+    while (1)
+    {
+        printf("1--存钱");
+        printf("2--区钱");
+        printf("3--余额");
+        printf("4--退出");
+        int a;
+        scanf（ "%d", &a）;
+        if (a==1)
+        {
+            
+        }
+        if (a==2)
+        {
+            
+        }
+        if (a==3)
+        {
+            
+        }
+        if (a==4)
+        {
+            
+        }
+    }
+
+    return 0;
+}
